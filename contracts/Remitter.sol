8dd36deb8e04bb861f4366840ea9da70dfc5038f
Peng Jie <bivinity.pengzjie@gmail.com>
2018-11-01 12:31:37 +0800

    fix(remitter): code review update

--- contracts/Remitter.sol
+++ contracts/Remitter.sol
@@ -26,8 +26,8 @@ contract Remitter is Ownable {
         address exchanger,
         bytes32 password1,
         bytes32 password2
-    ) public pure returns (bytes32) {
-        return keccak256(abi.encodePacked(exchanger, password1, password2));
+    ) public view returns (bytes32) {
+        return keccak256(abi.encodePacked(this, exchanger, password1, password2));
     }
 
     function sendFund(
@@ -35,9 +35,11 @@ contract Remitter is Ownable {
         bytes32 passwordHash,
         uint blockDuration
     ) public payable returns (bool) {
+        Remittance storage remittance = remittances[passwordHash];
+
         require(msg.value > 0, "msg.value should be greater than 0");
         require(exchanger != address(0), "exchanger address can't equals 0");
-        require(remittances[passwordHash].balance == 0, "remittances balance should be equals 0");
+        require(remittance.balance == 0, "remittances balance should be equals 0");
         require(blockDuration > 0, "block duration should be less than 0");
         require(
             blockDuration <= ONE_YEAR_OF_BLOCKS,
@@ -46,9 +48,9 @@ contract Remitter is Ownable {
 
         uint lastBlock = block.number + blockDuration;
 
-        remittances[passwordHash].exchanger = exchanger;
-        remittances[passwordHash].balance = msg.value;
-        remittances[passwordHash].lastBlock = lastBlock;
+        remittance.exchanger = exchanger;
+        remittance.balance = msg.value;
+        remittance.lastBlock = lastBlock;
 
         emit LogSendFund(msg.sender, exchanger, passwordHash, msg.value, blockDuration);
 
@@ -64,13 +66,15 @@ contract Remitter is Ownable {
             password1,
             password2
         );
-        uint balance = remittances[passwordHash].balance;
+        Remittance storage remittance = remittances[passwordHash];
 
-        require(password1.length != 0, "password1 is required");
-        require(password2.length != 0, "password2 is required");
-        require(balance > 0, "Nothing to withdraw, balance equals 0");
+        uint balance = remittance.balance;
+        assert(balance > 0);
 
-        remittances[passwordHash].balance = 0;
+        remittance.exchanger = address(0);
+        remittance.balance = 0;
+        remittance.lastBlock = 0;
+
         emit LogWithdraw(msg.sender, balance);
         msg.sender.transfer(balance);
 
--- test/remittance.test.js
+++ test/remittance.test.js
@@ -1,4 +1,5 @@
 import { expect } from 'chai';
+import { BigNumber } from 'bignumber.js';
 import web3Utils from 'web3-utils';
 import { default as Promise } from 'bluebird';
 import expectedException from '../utils/expectedException';
@@ -29,7 +30,7 @@ contract('Remitter', accounts => {
 
   describe('remittance function', () => {
     it('should fail if msg.value equals 0', async () => {
-      const value = web3.toWei('0', 'ether');
+      const value = web3Utils.toWei('0', 'ether');
 
       await expectedException(
         () => remitter.sendFund(bob, passwordHash, 1, { from: alice, value }),
@@ -38,7 +39,7 @@ contract('Remitter', accounts => {
     });
 
     it('should fail if exchanger address equals address(0)', async () => {
-      const value = web3.toWei('0.1', 'ether');
+      const value = web3Utils.toWei('0.1', 'ether');
 
       await expectedException(
         () => remitter.sendFund('0x0', passwordHash, 1, { from: alice, value }),
@@ -47,7 +48,7 @@ contract('Remitter', accounts => {
     });
 
     it('should fail if remittance balance not equals 0', async () => {
-      const value = web3.toWei('0.1', 'ether');
+      const value = web3Utils.toWei('0.1', 'ether');
 
       await remitter.sendFund(bob, passwordHash, 1, {
         from: alice,
@@ -65,7 +66,7 @@ contract('Remitter', accounts => {
     });
 
     it('should fail if duration equals 0', async () => {
-      const value = web3.toWei('0.1', 'ether');
+      const value = web3Utils.toWei('0.1', 'ether');
 
       await expectedException(
         () =>
@@ -113,6 +114,16 @@ contract('Remitter', accounts => {
   });
 
   describe('withdraw function', () => {
+    it('should fail if remittance balances is empty', async () => {
+      await expectedException(
+        () =>
+          remitter.withdraw(bobPassword, carolPassword, {
+            from: bob
+          }),
+        3000000
+      );
+    });
+
     it('should fail if password equals empty', async () => {
       const value = web3Utils.toWei('0.1', 'ether');
 
@@ -151,7 +162,9 @@ contract('Remitter', accounts => {
       expect(log.event).to.equal('LogWithdraw');
       expect(log.args.from).to.equal(bob);
       expect(log.args.balance.toString(10)).to.equal(value.toString(10));
+      expect(BigNumber(remittanceAfter[0]).toString(10)).to.equal('0');
       expect(remittanceAfter[1].toString(10)).to.equal('0');
+      expect(remittanceAfter[2].toString(10)).to.equal('0');
     });
   });
 });
